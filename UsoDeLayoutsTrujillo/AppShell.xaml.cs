7e5f51e8239fe0eb6c337e3e88bc3a568121c0c9
Altermanns <isaac.277.trujillo@gmail.com>
2024-11-15 11:35:23 -0500

    Agregar archivos de proyecto.

---
+++ UsoDeLayoutsTrujillo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UsoDeLayoutsTrujillo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UsoDeLayoutsTrujillo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UsoDeLayoutsTrujillo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
