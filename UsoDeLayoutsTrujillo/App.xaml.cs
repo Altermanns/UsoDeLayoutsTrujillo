013e7e16d8663d428f3a5645787e198409759d3f
Altermanns <isaac.277.trujillo@gmail.com>
2024-11-15 18:13:24 -0500

    Creacion del Gridlayout y su funcionalidad

--- UsoDeLayoutsTrujillo/App.xaml.cs
+++ UsoDeLayoutsTrujillo/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new Stacklayoutdemo();
+            MainPage = new NavigationPage(new GridlayoutPage()); ;
         }
     }
 }
